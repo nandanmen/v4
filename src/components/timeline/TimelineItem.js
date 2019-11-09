e51b95e446b1e74e2545736a4342f7e47225f8db
Nanda <n.syahrasyad.n@gmail.com>
2019-11-09 15:25:57 -0800

    Added date text

--- src/components/timeline/TimelineItem.js
+++ src/components/timeline/TimelineItem.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { format } from "date-fns"
 
 import Heading from "@styles/Heading"
 import Text from "@styles/Text"
@@ -10,7 +11,7 @@ const { colors, space } = theme
 const TimelineItem = ({ html, endDate, startDate, title }) => (
   <div>
     <Header>
-      <Text variant="secondary">{new Date(startDate).toDateString()}</Text>
+      <Text variant="secondary">{getDateText(startDate, endDate)}</Text>
       <Heading variant="h3">{title}</Heading>
     </Header>
     <article dangerouslySetInnerHTML={{ __html: html }} />
@@ -18,6 +19,16 @@ const TimelineItem = ({ html, endDate, startDate, title }) => (
 )
 
 export default TimelineItem
+
+const getDateText = (startDate, endDate) => {
+  const start = new Date(startDate)
+  const end = new Date(endDate)
+
+  if (end.getFullYear() === 9999) {
+    return `${format(start, "MMMM yyyy")} — Present`
+  }
+  return `${format(start, "MMMM yyyy")} — ${format(end, "MMMM yyyy")}`
+}
 
 const Header = styled.header`
   margin-bottom: ${space[3]};
