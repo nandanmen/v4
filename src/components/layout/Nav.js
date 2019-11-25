0478994d8f9686fd32458db030b3cdc26c8ec54c
Nanda <n.syahrasyad.n@gmail.com>
2019-11-24 21:39:06 -0800

    Added nav styling

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,16 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import { Link as BaseLink } from "gatsby"
+
+import theme from "@styles/theme"
 
 const Nav = ({ pages }) => (
-  <nav>
-    <ul>
+  <NavContainer>
+    <NavItemsContainer>
       {pages.map(({ name, path }) => (
-        <li key={path}>
+        <NavItem key={path}>
           <Link to={path}>{name}</Link>
-        </li>
+        </NavItem>
       ))}
-    </ul>
-  </nav>
+    </NavItemsContainer>
+  </NavContainer>
 )
 
 export default Nav
+
+const NavContainer = styled.nav`
+  padding: ${theme.space[3]}px;
+`
+
+const NavItemsContainer = styled.ul`
+  align-items: center;
+  display: flex;
+
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  margin-right: ${theme.space[2]}px;
+  opacity: 0;
+
+  &:last-child {
+    margin-left: auto;
+    margin-right: 0;
+    opacity: 1;
+  }
+`
+
+const Link = styled(BaseLink)`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes[0]}px;
+  font-weight: ${theme.fontWeights.semi};
+  text-decoration: none;
+`
