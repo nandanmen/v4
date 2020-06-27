8d769a04ef346290ee849e04a85d315040db1366
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-06-26 22:50:09 -0700

    Update colors and add social icons

--- src/components/Module.js
+++ src/components/Module.js
@@ -1,9 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 
-export default function Module({ module }) {
+export default function Module({ module, color }) {
   return (
-    <ModuleWrapper>
+    <ModuleWrapper bg={color}>
       <h1>{module.frontmatter.title}</h1>
       <p>{module.frontmatter.description}</p>
     </ModuleWrapper>
@@ -11,7 +11,7 @@ export default function Module({ module }) {
 }
 
 const ModuleWrapper = styled.div`
-  background: #2E4756;
+  background: ${props => props.bg};
   border-radius: 1em;
   padding: 2em;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import styled from "styled-components"
+import { Github, LinkedinSquare, Twitter } from "@styled-icons/boxicons-logos"
 
 import Layout from "@components/Layout"
 import Module from "@components/Module"
 import useModules from "@utils/useModules"
+
+const colors = ["#b5838d", "#6d6875", "#6930c3", "#457b9d"]
 
 export default function IndexPage() {
   const modules = useModules()
@@ -12,13 +15,24 @@ export default function IndexPage() {
       <About>
         <h1>Hi! I'm Nanda.</h1>
         <p>
-          I'm a software developer @ Tapestry, tech lead @ UBC Launchpad and 4th
+          I'm a senior developer @ Tapestry, tech lead @ UBC Launchpad and 5th
           year Business and Computer Science @ UBC.
         </p>
+        <Socials>
+          <li>
+            <Github size="32" />
+          </li>
+          <li>
+            <LinkedinSquare size="32" />
+          </li>
+          <li>
+            <Twitter size="32" />
+          </li>
+        </Socials>
       </About>
       <Modules>
-        {modules.map(module => (
-          <Module module={module} />
+        {modules.map((module, index) => (
+          <Module color={colors[index]} module={module} />
         ))}
       </Modules>
     </Layout>
@@ -35,4 +49,18 @@ const Modules = styled.section`
   grid-template-rows: repeat(2, 1fr);
   grid-column: span 4 / -1;
   grid-gap: 1em;
+`
+
+const Socials = styled.ul`
+  list-style: none;
+  display: flex;
+  margin-top: 32px;
+
+  > * {
+    margin-right: 12px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
 `
